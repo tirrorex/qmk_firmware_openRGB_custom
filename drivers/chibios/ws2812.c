acadcc8d172dbae72c364545749c3e9d02d43e63
jath03 <jackthughes@yahoo.com>
2021-04-24 16:58:18 -0700

    Reduce nops per loop to work with sonix chips

--- drivers/chibios/ws2812.c
+++ drivers/chibios/ws2812.c
@@ -8,6 +8,8 @@
 #ifndef NOP_FUDGE
 #    if defined(STM32F0XX) || defined(STM32F1XX) || defined(STM32F3XX) || defined(STM32F4XX) || defined(STM32L0XX)
 #        define NOP_FUDGE 0.4
+#    elif defined(SN32F24xx)
+#        define NOP_FUDGE 0.03
 #    else
 #        error("NOP_FUDGE configuration required")
 #        define NOP_FUDGE 1  // this just pleases the compile so the above error is easier to spot
@@ -22,8 +24,7 @@
 #    define WS2812_OUTPUT_MODE PAL_MODE_OUTPUT_OPENDRAIN
 #endif
 
-#define NUMBER_NOPS 6
-#define CYCLES_PER_SEC (STM32_SYSCLK / NUMBER_NOPS * NOP_FUDGE)
+#define CYCLES_PER_SEC (STM32_SYSCLK * NOP_FUDGE)
 #define NS_PER_SEC (1000000000L)  // Note that this has to be SIGNED since we want to be able to check for negative values of derivatives
 #define NS_PER_CYCLE (NS_PER_SEC / CYCLES_PER_SEC)
 #define NS_TO_CYCLES(n) ((n) / NS_PER_CYCLE)
@@ -31,12 +32,7 @@
 #define wait_ns(x)                                  \
     do {                                            \
         for (int i = 0; i < NS_TO_CYCLES(x); i++) { \
-            __asm__ volatile("nop\n\t"              \
-                             "nop\n\t"              \
-                             "nop\n\t"              \
-                             "nop\n\t"              \
-                             "nop\n\t"              \
-                             "nop\n\t");            \
+            __asm__ volatile("nop\n\t");            \
         }                                           \
     } while (0)
 
