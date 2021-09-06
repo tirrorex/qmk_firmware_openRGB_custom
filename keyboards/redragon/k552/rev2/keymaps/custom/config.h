54b4c05e0642d4a8ce2735bc37405cdcd5080485
jath03 <jackthughes@yahoo.com>
2021-09-06 01:03:15 -0700

    Fixes

--- keyboards/redragon/k552/rev2/keymaps/custom/config.h
+++ keyboards/redragon/k552/rev2/keymaps/custom/config.h
@@ -1,4 +1,4 @@
-                            /* Copyright 2020 Adam Honse <calcprogrammer1@gmail.com>
+/* Copyright 2020 Adam Honse <calcprogrammer1@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,28 +19,31 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x0C45
-#define PRODUCT_ID      0x5004
-#define DEVICE_VER      0x0001
+#define VENDOR_ID 0x0C45
+#define PRODUCT_ID 0x5004
+#define DEVICE_VER 0x0001
 
-#define MANUFACTURER    Redragon
-#define PRODUCT         K552
-#define DESCRIPTION     K552 Kumara
+#define MANUFACTURER Redragon
+#define PRODUCT K552
+#define DESCRIPTION K552 Kumara
 
 /* key matrix size */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 17
 
-#define DIODE_DIRECTION COL2ROW
-
-#define MATRIX_COL_PINS { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, B0, B1 }
-#define MATRIX_ROW_PINS { C15, D11, D10, D9, D8, D7 }
+#define MATRIX_COL_PINS \
+    { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, B0, B1 }
+#define MATRIX_ROW_PINS \
+    { C15, D11, D10, D9, D8, D7 }
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #ifdef DEBOUNCE
-#   undef DEBOUNCE
+#    undef DEBOUNCE
 #endif
 #define DEBOUNCE 10
+
+#undef DRIVER_LED_TOTAL
+#define DRIVER_LED_TOTAL 95
 
 #define RGB_MATRIX_KEYPRESSES
 // #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_SPLASH
@@ -54,10 +57,4 @@
 #define RGBW
 #define WS2812_TRST_US 100
 
-#define COMBO_COUNT 0
-// #define COMBO_TERM 1000
-
-// #define WS2812_BYTE_ORDER 0
-// #define WS2812_EXTERNAL_PULLUP
-//
 #define FORCE_NKRO
